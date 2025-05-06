e8d837beef1056a8b3748c4aa9a4cdc5bb971fd7
RobCar1987 <roberto.caridi@gmail.com>
2025-05-07 01:55:05 +0200

    correzione cors per locale

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173'], // o '*' per tutti (solo in sviluppo)
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
